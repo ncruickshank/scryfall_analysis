3d86bb7906e2ac3b36218b40bd52372bcae3661b
Nick Cruickshank <nccruickshank94@gmail.com>
2021-06-15 22:08:44 -0400

    Nearly finish plot
    Still need to figure out how to create the axs for colorless cards

--- scryfall_pioneer_commons.py
+++ scryfall_pioneer_commons.py
@@ -25,6 +25,7 @@ evasion_keywords = ['Flying', 'Trample', 'Menace',
                     'Plainswalk', 'Islandwalk', 'Forestwalk', 'Mountainwalk', 'Swampwalk',
                     'Skulk', 'Shadow', 'Fear', 'Intimidate']
 pioneer['evasion'] = pioneer['keywords'].apply(lambda x: bool(any(item in x for item in evasion_keywords)))
+#pioneer.color_identity = pioneer.color_identity.apply(lambda x: '[C]' if len(x)==0 else x)
 
 ## removal bool
 removal_regexes = [
@@ -68,65 +69,126 @@ combat_regexes = [
 
 pioneer['combat_trick'] = pioneer['oracle_text'].astype(str).apply(lambda x: bool(any(regex.search(x.lower()) for regex in combat_regexes)))
 
-# summary statistics by set
+# plot the data
+
+## reshape df
 card_type_breakdown = pioneer.groupby('set_name').size().reset_index(name = 'commons') # number of commons
 
+## define function to be used for each axs
 card_types = ['Creature', 'Enchantment', 'Instant', 'Sorcery', 'Artifact']
-for card_type in card_types:
-    new_df = pioneer[pioneer['type_line'].str.contains(card_type)].groupby('set_name').size().reset_index(name = card_type + "s")
-    card_type_breakdown = card_type_breakdown.merge(new_df, how = 'left', on = 'set_name')
+def creature_curve_variables(color_identity):
+    # take a slice of the pioneer df for the color identity
+    df = pioneer[pioneer['color_identity'].astype(str).str.contains(color_identity)]
+    
+    # establish card type breakdown and card type ratios
+    card_type_breakdown = df.groupby('set_name').size().reset_index(name = 'commons')
+    for card_type in card_types:
+        new_df = df[df['type_line'].str.contains(card_type)].groupby('set_name').size().reset_index(name = card_type + "s")
+        card_type_breakdown = card_type_breakdown.merge(new_df, how = 'left', on = 'set_name')
+    card_type_breakdown = card_type_breakdown.replace(np.nan, 0).set_index('set_name')
+    ctype_ratio = card_type_breakdown.iloc[:, 1:].div(card_type_breakdown.commons, axis = 0)
+    
+    # creatures
+    creatures = df[df['type_line'].str.contains('Creature')]
+    noncreatures = df[~df['type_line'].str.contains('Creature')]
+    creature_curve_df = creatures.groupby(['set_name', 'cmc']).size().reset_index(name = 'cards')
+    creature_curve_df = creature_curve_df.groupby('cmc').cards.agg(['mean', 'min', 'max', 'std']).reset_index()
+    creature_curve_df['count_ratio'] = creature_curve_df['mean'] / creature_curve_df['mean'].sum()
+    
+    # additional values
+    creature_ratio = ctype_ratio['Creatures'].mean()
+    creatures_w_evasion = len(creatures[creatures['evasion'] == True]) / len(creatures)
+    noncreature_removal_spell_ratio = len(noncreatures[noncreatures['removal'] == True]) / len(noncreatures)
+    noncreature_combat_trick_ratio = len(noncreatures[noncreatures['combat_trick'] == True]) / len(noncreatures)
+    
+    return creature_ratio, creatures_w_evasion, creature_curve_df
 
-card_type_breakdown = card_type_breakdown.replace(np.nan, 0)
-
+## set up the plot
 fig, axs = plt.subplots(2,3)
 fig.set_figheight(15)
 fig.set_figwidth(30)
 fig.suptitle('MTG Pioneer Sets: Common Creature Curve', fontsize = 20)
 
 ## subplot 1: white
+creature_ratio_w, creatures_w_evasion_w, creature_curve_df_w = creature_curve_variables('[W]')
 
-## white dfs
-white = pioneer[pioneer['color_identity'].astype(str).str.contains("W")]
-card_type_breakdown_w = white.groupby('set_name').size().reset_index(name = 'commons') # number of commons
-
-card_types = ['Creature', 'Enchantment', 'Instant', 'Sorcery', 'Artifact']
-for card_type in card_types:
-    new_df = white[white['type_line'].str.contains(card_type)].groupby('set_name').size().reset_index(name = card_type + "s")
-    card_type_breakdown_w = card_type_breakdown_w.merge(new_df, how = 'left', on = 'set_name')
-
-card_type_breakdown_w = card_type_breakdown_w.replace(np.nan, 0).set_index('set_name')
-ctypes_ratio_w = card_type_breakdown_w.iloc[:, 1:].div(card_type_breakdown_w.commons, axis = 0)
-#ratio_sums = [row.Creatures + row.Enchantments + row.Instants + row.Sorcerys + row.Artifacts for index, row in ctypes_ratio_w.iterrows()]
-#ctypes_ratio_w['ratio_sums'] = ratio_sums
-
-white_creatures = white[white['type_line'].str.contains('Creature')]
-creature_curve_df_w = white_creatures.groupby(['set_name', 'cmc']).size().reset_index(name = 'cards')
-creature_curve_df_w = creature_curve_df_w.groupby('cmc').cards.agg(['mean', 'min', 'max', 'std']).reset_index()
-creature_curve_df_w['count_ratio'] = creature_curve_df_w['mean'] / creature_curve_df_w['mean'].sum()
-
-creature_ratio_w = ctypes_ratio_w['Creatures'].mean()
-creatures_w_evasion = len(white_creatures[white_creatures['evasion'] == True]) / len(white_creatures)
-
+### set axis title
 axs[0,0].set_title('WHITE: {} of commons are creatures. {} of creatures have evasion.'.
-              format(str(round(100 * creature_ratio_w)) + '%', str(round(100*creatures_w_evasion)) + '%'))
+              format(str(round(100 * creature_ratio_w)) + '%', str(round(100*creatures_w_evasion_w)) + '%'))
+
+### plot curve
 axs[0,0].bar(x = creature_curve_df_w['cmc'], height = creature_curve_df_w['mean'], yerr = creature_curve_df_w['std'],
             color = 'ivory', edgecolor = 'black')
+
+### add text to bars
 for index, row in creature_curve_df_w.iterrows():
     axs[0,0].text(row['cmc'], 0.5, 'Ratio:', horizontalalignment = 'center')
     axs[0,0].text(row['cmc'], 0.25, round(row['count_ratio'], 2), horizontalalignment = 'center')
 
 
 ## subplot 2: blue
-axs[0,1].set_title('Blue')
+creature_ratio_u, creatures_w_evasion_u, creature_curve_df_u = creature_curve_variables('[U]')
+
+### set axis title
+axs[0,1].set_title('BLUE: {} of commons are creatures. {} of creatures have evasion.'.
+              format(str(round(100 * creature_ratio_u)) + '%', str(round(100*creatures_w_evasion_u)) + '%'))
+
+### plot curve
+axs[0,1].bar(x = creature_curve_df_u['cmc'], height = creature_curve_df_u['mean'], yerr = creature_curve_df_u['std'],
+            color = 'skyblue', edgecolor = 'black')
+
+### add text to bars
+for index, row in creature_curve_df_u.iterrows():
+    axs[0,1].text(row['cmc'], 0.5, 'Ratio:', horizontalalignment = 'center')
+    axs[0,1].text(row['cmc'], 0.25, round(row['count_ratio'], 2), horizontalalignment = 'center')
 
 ## subplot 3: black
-axs[0,2].set_title('Black')
+creature_ratio_b, creatures_w_evasion_b, creature_curve_df_b = creature_curve_variables('[B]')
+
+### set axis title
+axs[0,2].set_title('BLACK: {} of commons are creatures. {} of creatures have evasion.'.
+              format(str(round(100 * creature_ratio_b)) + '%', str(round(100*creatures_w_evasion_b)) + '%'))
+
+### plot curve
+axs[0,2].bar(x = creature_curve_df_b['cmc'], height = creature_curve_df_b['mean'], yerr = creature_curve_df_b['std'],
+            color = 'darkorchid', edgecolor = 'black')
+
+### add text to bars
+for index, row in creature_curve_df_b.iterrows():
+    axs[0,2].text(row['cmc'], 0.5, 'Ratio:', horizontalalignment = 'center')
+    axs[0,2].text(row['cmc'], 0.25, round(row['count_ratio'], 2), horizontalalignment = 'center')
 
 ## subplot 4: red
-axs[1,0].set_title('Red')
+creature_ratio_r, creatures_w_evasion_r, creature_curve_df_r = creature_curve_variables('[R]')
+
+### set axis title
+axs[1,0].set_title('RED: {} of commons are creatures. {} of creatures have evasion.'.
+              format(str(round(100 * creature_ratio_r)) + '%', str(round(100*creatures_w_evasion_r)) + '%'))
+
+### plot curve
+axs[1,0].bar(x = creature_curve_df_r['cmc'], height = creature_curve_df_r['mean'], yerr = creature_curve_df_r['std'],
+            color = 'crimson', edgecolor = 'black')
+
+### add text to bars
+for index, row in creature_curve_df_r.iterrows():
+    axs[1,0].text(row['cmc'], 0.5, 'Ratio:', horizontalalignment = 'center')
+    axs[1,0].text(row['cmc'], 0.25, round(row['count_ratio'], 2), horizontalalignment = 'center')
 
 ## subplot 5: green
-axs[1,1].set_title('Green')
+creature_ratio_g, creatures_w_evasion_g, creature_curve_df_g = creature_curve_variables('[G]')
+
+### set axis title
+axs[1,1].set_title('GREEN: {} of commons are creatures. {} of creatures have evasion.'.
+              format(str(round(100 * creature_ratio_g)) + '%', str(round(100*creatures_w_evasion_g)) + '%'))
+
+### plot curve
+axs[1,1].bar(x = creature_curve_df_g['cmc'], height = creature_curve_df_g['mean'], yerr = creature_curve_df_g['std'],
+            color = 'springgreen', edgecolor = 'black')
+
+### add text to bars
+for index, row in creature_curve_df_g.iterrows():
+    axs[1,1].text(row['cmc'], 0.5, 'Ratio:', horizontalalignment = 'center')
+    axs[1,1].text(row['cmc'], 0.25, round(row['count_ratio'], 2), horizontalalignment = 'center')
 
 ## subplot 6: colorless
 axs[1,2].set_title('Colorless')
